02834d58971a9e66603a7ea525073935039a5fef
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2023-06-25 15:35:37 -0500

    Update HawkenResolutionChanger.bat
    revert change. 
    
    something doesn't work right.

--- HawkenResolutionChanger.bat
+++ HawkenResolutionChanger.bat
@@ -1,14 +1,8 @@
 @echo off
 cls
 :type splash.txt
-timeout /t 3 >nul
-
-if exist resolution.txt (
-    set /p choice=<resolution.txt
-    goto CHOICE
-) else (
-    goto MENU
-)
+:timeout /t 3 >nul
+goto MENU
 
 :MENU
 cls
@@ -23,11 +17,8 @@ echo 4) 1920x1080
 echo 5) 2560x1440
 echo 6) 3840x2160
 echo 7) Exit
-echo 8) Change Resolution
 echo ==================================
-set /p choice=Enter your choice (1-8):
-
-:CHOICE
+set /p choice=Enter your choice (1-7):
 if "%choice%" == "1" set resx=1280&set resy=720&goto RUN
 if "%choice%" == "2" set resx=1366&set resy=768&goto RUN
 if "%choice%" == "3" set resx=1600&set resy=900&goto RUN
@@ -35,12 +26,9 @@ if "%choice%" == "4" set resx=1920&set resy=1080&goto RUN
 if "%choice%" == "5" set resx=2560&set resy=1440&goto RUN
 if "%choice%" == "6" set resx=3840&set resy=2160&goto RUN
 if "%choice%" == "7" goto EXIT
-if "%choice%" == "8" del resolution.txt&goto MENU
-echo Invalid choice. Try again.
 goto MENU
 
 :RUN
-echo %choice% > resolution.txt
 echo Launching Hawken with %resx%x%resy% resolution...
 start "" "%~dp0HawkenGame-Win32-Shipping.exe" -FULLSCREEN -resx=%resx% -resy=%resy%
 goto EXIT
