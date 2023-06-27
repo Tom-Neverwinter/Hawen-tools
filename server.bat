b47ab487c1699efc09859fab5a8a087668a22740
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2023-06-27 11:26:13 -0500

    Update server.bat
    make it so you cant break the server player count that is hard coded

--- server.bat
+++ server.bat
@@ -16,8 +16,19 @@ if "%mapChoice%"=="2" set map=Andromeda_DM
 if "%mapChoice%"=="3" set map=Andromeda_Siege
 if "%mapChoice%"=="4" set map=Sahara_DM_E3
 
+:players
 echo.
 set /p numPlayers=Enter the number of players (1-32): 
+
+:: Validate number of players
+if %numPlayers% lss 1 (
+  echo Number of players should be between 1 and 32. Please try again.
+  goto players
+)
+if %numPlayers% gtr 32 (
+  echo Number of players should be between 1 and 32. Please try again.
+  goto players
+)
 
 echo.
 set /p goalScore=Enter the goal score: 
@@ -34,9 +45,11 @@ set /p gameMode=Enter the game mode number:
 if "%gameMode%"=="1" set game=Robots.R_TeamDeathmatch
 if "%gameMode%"=="2" set game=Robots.R_Deathmatch
 
+echo.
+set /p serverName=Enter the server name: 
+
 :start_server
 echo Starting Hawken server with the selected options...
-start HawkenGame-Win32-Shipping.exe Server %map%?NumPublicConnections=%numPlayers%?GoalScore=%goalScore%?TimeLimit=%timeLimit%?bIsLanMatch=True?game=%game% -log
+start HawkenGame-Win32-Shipping.exe Server %map%?NumPublicConnections=%numPlayers%?GoalScore=%goalScore%?TimeLimit=%timeLimit%?bIsLanMatch=True?game=%game%?ServerName=%serverName% -log
 echo Server started.
 pause
-
